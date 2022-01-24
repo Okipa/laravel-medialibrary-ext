b7ea781b7c6973b5ca6278f9e62834754d4bc39a
Arthur LORENT <arthur@acid.fr>
2022-01-24 17:06:20 +0100

    V10

--- tests/MediaLibraryExtTestCase.php
+++ tests/MediaLibraryExtTestCase.php
@@ -7,6 +7,7 @@ use Spatie\MediaLibrary\MediaLibraryServiceProvider;
 
 class MediaLibraryExtTestCase extends TestCase
 {
+    /** @SuppressWarnings(PHPMD.UnusedFormalParameter) */
     protected function getPackageProviders($app): array
     {
         return [MediaLibraryServiceProvider::class];
